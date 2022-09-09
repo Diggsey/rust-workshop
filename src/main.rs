a7f1e3b91b7fd120ea6241a1ca23bf0777034a20
Diggsey <diggsey@googlemail.com>
2022-09-09 19:46:47 +0100

    Step 2

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,55 @@
-// Comments written using "triple slash" are documentation comments. Documentation
-// for a crate can be generated automatically using `cargo doc`:
+use std::ops::Add;
+
 /// A structure to represent 3D vectors
 struct Vec3 {
-    // We define three floating-point fields for X/Y/Z components of the vector.
-    // `f32` means a 32-bit float, also known as a "single" in some languages.
     x: f32,
     y: f32,
     z: f32,
 }
 
-// Implement an inherent method for this structure.
 impl Vec3 {
     /// Calculate the length using Pythagoras' theorem
-    // The `self` parameter is written explicitly, even for inherent methods. There
-    // are several forms which the `self` parameter can take: this form means that
-    // the method does not modify the structure.
     fn length(&self) -> f32 {
-        // Fields are accessed via the `self` parameter. Simply writing `x` would not
-        // work.
         let sum_of_squares = self.x.powi(2) + self.y.powi(2) + self.z.powi(2);
 
-        // The last expression in a function is returned implicitly.
         sum_of_squares.sqrt()
     }
 }
 
-// This is the entry point for our program
-fn main() {
-    // Construct an instance of our vector type
-    let vec = Vec3 {
-        x: 2.0,
-        y: 3.0,
-        z: 6.0,
-    };
-    let length = vec.length();
+// When we want to share behaviours across multiple types, we use "traits". These
+// work similarly to "interfaces" in other languages. There are some special traits
+// which control the behaviour of operators, like the + operator. If we want to
+// support such an operator on our own types, we need only implement the requisite
+// trait.
+// In this case, we implement the "Add" trait.
+impl Add for Vec3 {
+    /// The result of adding two 3D vectors is another 3D vector.
+    type Output = Vec3;
 
-    // Print out the length of the vector
+    fn add(self, rhs: Self) -> Self::Output {
+        // Vectors are added component-wise
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+fn main() {
+    let vec1 = Vec3 {
+        x: 1.0,
+        y: 4.0,
+        z: 2.0,
+    };
+    let vec2 = Vec3 {
+        x: 1.0,
+        y: 2.0,
+        z: 7.0,
+    };
+
+    // Now we can use the `+` operator on our vectors.
+    let length = (vec1 + vec2).length();
+
     println!("Length: {length}");
 }
